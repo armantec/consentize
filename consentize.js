17c7a3f5595b31c15a6c4e72e81f2eda134813dc
armantec <ravikant.k.sharma@gmail.com>
2025-04-02 19:11:36 +0530

    Removed email address and name fields

--- consentize.js
+++ consentize.js
@@ -1,16 +1,16 @@
 
-var openConsentForm = function(options, name, email){
-$(document.body).append("<div id='consentbackdrop' style='position:fixed;top:0;left:0;width:100%;height:100%;background:#000;opacity:0.6' class='backdrop'></div>");
+var openConsentForm = function(options){
+$(document.body).append("<div id='consentbackdrop' class='backdrop'></div>");
 var url = "https://consentize.in/my-consent";
-url = url + "?p_p_id=com_armantec_consent_consent_form_portlet&_com_armantec_consent_consent_form_portlet_finderKey=readPolicies:readTerms:apiKey:name:emailAddress:consentType&_com_armantec_consent_consent_form_portlet_finderId=" + encodeURIComponent(options.policyPageUrl) + ":" + encodeURIComponent(options.termsPageUrl) + ":" + options.apiKey + ":" + name + ":" + email + ":" + options.consentType;
-$(document.body).append("<iframe id='consentwindow' style='width:calc(100% - 200px);height:700px;left:100px;top:10px;position:fixed;border:1px solid #ddd;' src='" + url + "'></iframe>");
+url = url + "?p_p_id=com_armantec_consent_consent_form_portlet&_com_armantec_consent_consent_form_portlet_finderKey=referenceNo::readPolicies::readTerms::apiKey::consentType&_com_armantec_consent_consent_form_portlet_finderId=" + options.referenceNo + "::" + encodeURIComponent(options.policyPageUrl) + "::" + encodeURIComponent(options.termsPageUrl) + "::" + options.apiKey + "::" + options.consentType;
+$(document.body).append("<iframe id='consentwindow' class='consentwindow'  src='" + url + "'></iframe>");
 }
 
 $(function(){
 
 $.fn.consentize = function(options){
    $(this).options = options;
-   $("<br/><input id='termscheck' value='' onclick='return false;' name='consentid' type='radio' required/> <label>I have read terms & conditions.</label><br/><br/>").insertBefore($(this).find("input[type=submit]"));
+   $("<br/><input style='float:left' id='termscheck' value='' onclick='return false;' name='consentid' type='radio' required/> <label for='termscheck' style='cursor: pointer;float: left;margin-left: 10px;margin-top: -3px;'>Your Consent for the Compliance of Digital Personal Data Protection Act (DPDP)</label><br/><br/>").insertBefore($(this).find("button[type=reset]"));
    $('#termscheck').click(function(){
    	  openConsentForm(options, this.form.elements[options.nameFieldName].value, this.form.elements[options.emailFieldName].value);
    })
